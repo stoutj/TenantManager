b4df34cd7d2c691833e2e3c1a6f7cbcc63d7fd56
Jim <stoutj734@gmail.com>
2013-07-18 01:21:45 -0400

    Adding Landlords

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,6 +2,7 @@ class Property < ActiveRecord::Base
   attr_accessible :address
 
   has_many :rental_apps
+  belongs_to :landlord
 
   validates :address, presence: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130709010653) do
+ActiveRecord::Schema.define(:version => 20130718051448) do
+
+  create_table "landlords", :force => true do |t|
+    t.string   "email"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.integer  "landlord_id"
+  end
 
   create_table "properties", :force => true do |t|
     t.string   "address"
